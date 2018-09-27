53f287f98ca34377d00bc785df0d8eb49389662d
Carlos Rocha <rocha.carlosmiguel@gmail.com>
2018-09-27 21:51:37 +0100

    code was accidently left behind

--- src/main/java/com/leetcode/maxincreasekeepcityskyline/Main.java
+++ src/main/java/com/leetcode/maxincreasekeepcityskyline/Main.java
@@ -1,15 +1,13 @@
-package com.leetcode.mapsumpairs;
+package com.leetcode.maxincreasekeepcityskyline;
 
 public class Main {
 
-    public static void main(String[] args)  {
-        MapSum ms = new MapSum();
-        ms.insert("aa",3);
-        ms.insert("aa",2);
+    public static void main(String[] args) {
 
-        int result = ms.sum("aa");
-        System.out.println("result : " + result);
-        System.out.println("result : 2");
+        int[][] grid = {{3, 0, 8, 4}, {2, 4, 5, 7}, {9, 2, 6, 3}, {0, 3, 1, 0}};
+
+        Solution s = new Solution();
+        System.out.println(s.maxIncreaseKeepingSkyline(grid));
 
     }
 }
--- src/main/java/com/leetcode/maxincreasekeepcityskyline/Solution.java
+++ src/main/java/com/leetcode/maxincreasekeepcityskyline/Solution.java
@@ -1,4 +1,53 @@
 package com.leetcode.maxincreasekeepcityskyline;
 
+import java.util.Arrays;
+
 public class Solution {
+    public int maxIncreaseKeepingSkyline(int[][] grid) {
+        final int numberOfColumns = grid[0].length;
+        final int numberOfRows = grid.length;
+
+        int[] leftRightHighest = new int[numberOfRows];
+        int[] topButtomHighest = new int[numberOfColumns];
+
+        // Initialize vectors
+        for (int x = 0; x < numberOfRows; x++) {
+            leftRightHighest[x] = -1;
+        }
+        for (int y = 0; y < numberOfColumns; y++) {
+            topButtomHighest[y] = -1;
+        }
+        // get current viewed from left or right
+        for (int x = 0; x < numberOfRows; x++) {
+            for (int y = 0; y < numberOfColumns; y++) {
+                if (leftRightHighest[x] < grid[x][y]) {
+                    leftRightHighest[x] = grid[x][y];
+                }
+                if (topButtomHighest[y] < grid[x][y]) {
+                    topButtomHighest[y] = grid[x][y];
+                }
+            }
+        }
+        //System.out.println("topButtomHighest: " + Arrays.toString(topButtomHighest));
+        //System.out.println("leftRightHighest : " + Arrays.toString(leftRightHighest));
+        int totalSum = 0;
+        for (int x = 0; x < numberOfRows; x++) {
+            int maxAllowedInRow = leftRightHighest[x];
+            for (int y = 0; y < numberOfColumns; y++) {
+                int maxAllowedIncollum = topButtomHighest[y];
+                int valueToAdd = Math.min(maxAllowedInRow, maxAllowedIncollum) - grid[x][y];
+                grid[x][y] = grid[x][y] + valueToAdd;
+
+                totalSum += valueToAdd;
+            }
+        }
+
+        // Print for debug
+        /*        for (int x = 0; x < numberOfRows; x++) {
+            System.out.println("new skyline : " + Arrays.toString(grid[x]));
+        }*/
+
+
+        return totalSum;
+    }
 }
