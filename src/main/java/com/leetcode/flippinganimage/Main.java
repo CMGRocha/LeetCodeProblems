f723c1de437c9dc71775a554490d0c8b6385b10d
Carlos Rocha <rocha.carlosmiguel@gmail.com>
2018-10-02 21:59:54 +0100

    832 missing main file

---
+++ src/main/java/com/leetcode/flippinganimage/Main.java
@@ -0,0 +1,15 @@
+package com.leetcode.flippinganimage;
+
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[][] image = {{1, 1, 0}, {1, 0, 1}, {0, 0, 0}};
+        System.out.println(Arrays.deepToString(solution.flipAndInvertImage(image)));
+
+
+    }
+}
