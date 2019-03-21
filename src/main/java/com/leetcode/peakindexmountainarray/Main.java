1832a1a0eccca5a35ede87708c9923faea340605
Carlos Rocha <rocha.carlosmiguel@gmail.com>
2019-03-21 19:49:29 +0000

    852. Peak Index in a Mountain Array

---
+++ src/main/java/com/leetcode/peakindexmountainarray/Main.java
@@ -0,0 +1,11 @@
+package com.leetcode.peakindexmountainarray;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        int[] array = {0, 2, 1, 0};
+        System.out.println(s.peakIndexInMountainArray(array));
+    }
+}
---
+++ src/main/java/com/leetcode/peakindexmountainarray/Solution.java
@@ -0,0 +1,18 @@
+package com.leetcode.peakindexmountainarray;
+
+public class Solution {
+
+    public int peakIndexInMountainArray(int[] array) {
+        int low = 0;
+        int high = array.length - 1;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (array[mid] < array[mid + 1]) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+        return low;
+    }
+}
