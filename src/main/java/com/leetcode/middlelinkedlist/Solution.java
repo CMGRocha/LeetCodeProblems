0d7c100a014527edc8f9afebf55c337070863f53
Carlos Rocha <rocha.carlosmiguel@gmail.com>
2019-04-03 20:59:54 +0100

    876. Middle of the Linked List

---
+++ src/main/java/com/leetcode/middlelinkedlist/Solution.java
@@ -0,0 +1,17 @@
+package com.leetcode.middlelinkedlist;
+
+public class Solution {
+    public ListNode middleNode(ListNode head) {
+
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while (fast != null && fast.next != null) {
+
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+
+        return slow;
+    }
+}
